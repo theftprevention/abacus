64a56f517f7045a96edead8bf481b02c9f0be144
Jacob McCollum <jacob@jacobmccollum.com>
2024-03-24 10:16:33 -0400

    Update tests

--- test/crawler-stack.test.ts
+++ test/crawler-stack.test.ts
@@ -1,24 +1,26 @@
 import { App } from 'aws-cdk-lib';
 import { Template } from 'aws-cdk-lib/assertions';
-import { Architecture, Runtime } from 'aws-cdk-lib/aws-lambda';
-import { CrawlerStack } from '../crawler/lib/crawler-stack';
+import { Runtime } from 'aws-cdk-lib/aws-lambda';
+import { CrawlerStack } from '../src/crawler/crawlerStack';
 
 describe('CrawlerStack', () => {
-  test('synthesizes correctly', () => {
+  it('synthesizes correctly', () => {
     const app = new App();
 
     // Create the CrawlerStack.
-    const crawlerStack = new CrawlerStack(app, 'CrawlerStack');
+    const crawlerStack = new CrawlerStack(app, 'TestCrawlerStack');
 
     // Prepare the stack for assertions.
     const template = Template.fromStack(crawlerStack);
 
-    // Assert it creates the function with the correct properties...
-    template.resourceCountIs('AWS::Lambda::Function', 1);
-    template.hasResourceProperties('AWS::Lambda::Function', {
-      Architectures: [Architecture.X86_64.name],
-      Handler: 'index.main',
-      Runtime: Runtime.NODEJS_20_X.name,
-    });
+    // Assert it creates the functions with the correct properties...
+    template.resourceCountIs('AWS::Lambda::Function', 5);
+
+    for (const step of ['beginCrawl', 'enqueueUrls', 'getProductGroup', 'nextExecution', 'stopCrawl']) {
+      template.hasResourceProperties('AWS::Lambda::Function', {
+        Handler: `index.${step}`,
+        Runtime: Runtime.NODEJS_20_X.name,
+      });
+    }
   });
 });
